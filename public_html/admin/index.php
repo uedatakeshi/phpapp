59b1abb35b12dc48ac1ca01bcea43e5f2b1f2acc
Takeshi Ueda <uedatakeshi@gmail.com>
2017-01-06 18:21:25 +0900

    add webpack and vue

--- public_html/admin/index.php
+++ public_html/admin/index.php
@@ -6,7 +6,7 @@ logger()->debug('message', ['env' => $_ENV['MY_PHP_ENV']]);
  * 自作クラス namespace なしだとuseする必要なし
  */
 $exam = new Exam;
-$exam->index();
+//$exam->index();
 
 
 $pdo->getProfiler()->setActive(true);
@@ -19,29 +19,36 @@ $bind = [
 // the statement to prepare
 $result = $pdo->fetchAll($query, $bind);
 
-var_dump($result);
+//var_dump($result);
 
 foreach ($pdo->yieldAssoc($query, $bind) as $key => $row) {
     // ...
-	echo $key;
-	var_dump($row);
+//	echo $key;
+//	var_dump($row);
 }
 
 
 
 foreach ($pdo->getProfiler()->getProfiles() as $i => $profile) {
-	var_dump($profile);
+//	var_dump($profile);
 }
 
 $template = $twig->load('admin/index.html');
 //echo $template->render(array('php_env' => $_ENV['MY_PHP_ENV']));
 $data = array('php_env' => $_ENV['MY_PHP_ENV'], 'name' => $_ENV['NAME']);
 //echo $template->render(array('php_env' => $_ENV['MY_PHP_ENV'], 'name' => $_ENV['NAME']));
-$template->display($data);
+//$template->display($data);
 ?>
 <!DOCTYPE html>
+<head>
 <title>WEB+DB PRESS Vol.96 PHP大規模開発入門 第17回 サンプルコード</title>
-<link rel="stylesheet" href="/style/sample.css">
+</head>
+<body>
+<div id="demo">
+  <div>{{message}}</div>
+  <input v-model="message">
+</div>
+<script type="text/javascript" src="js/bundle.js" charset="utf-8"></script>
 <h1>PHP大規模開発入門 第17回 サンプルコード</h1>
 <h2>脆弱性のあるコード例</h2>
 <ul>
@@ -61,3 +68,5 @@ $template->display($data);
 <?php else: ?>
     <p><code>$_ENV['MY_PHP_ENV']</code>がセットされてません。<code class="file">.env</code>ファイルを作成してください。</p>
 <?php endif; ?>
+</body>
+</html>
